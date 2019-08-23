e315545fb751ea27d1637e0fd1e9366ec92e3e06
rwoo <rwoo@bistel.com>
2019-08-23 17:10:56 +0900

    #25-korean-version-2 ``` modified:   99_Utility/01_remove_tmp_files.sh modified:   examples/resume-0.png modified:   examples/resume-1.png modified:   examples/resume.pdf modified:   examples/resume.tex modified:   examples/resume/summary.tex modified:   examples/resume/summary_korean.tex modified:   examples/resume_korean.pdf modified:   examples/resume_korean.tex ```

--- 99_Utility/01_remove_tmp_files.sh
+++ 99_Utility/01_remove_tmp_files.sh
@@ -8,8 +8,11 @@ else
 	echo "Remove tmp files"
 	rm -rf examples/missfont.log
 	rm -rf examples/resume.aux
+	rm -rf examples/resume_korean.aux
 	rm -rf examples/resume.log
+	rm -rf examples/resume_korean.log
 	rm -rf examples/resume.out
+	rm -rf examples/resume_korean.out
 	rm -rf examples/resume/education.log	
 	rm -rf examples/resume/project.log	
 	rm -rf examples/resume.synctex.gz
