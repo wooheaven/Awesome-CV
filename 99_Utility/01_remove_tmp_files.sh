cc4141d1097ae6b8c7b3225a3fcb27e398074f3b
ubuntu <wooheaven79@gmail.com>
2019-10-01 19:33:07 +0900

    #37-merge-resume-resuem_korean ``` modified:   99_Utility/01_remove_tmp_files.sh new file:   examples/merge_resume.pdf new file:   examples/merge_resume.tex ```

--- 99_Utility/01_remove_tmp_files.sh
+++ 99_Utility/01_remove_tmp_files.sh
@@ -7,17 +7,27 @@ if [ $process_TeXShop_CNT -gt 0 ] ; then
 else
 	echo "Remove tmp files"
 	rm -rf examples/missfont.log
+
 	rm -rf examples/resume.aux
 	rm -rf examples/resume_korean.aux
+	rm -rf examples/merge_resume.aux
+
 	rm -rf examples/resume.log
 	rm -rf examples/resume_korean.log
+	rm -rf examples/merge_resume.log
+
 	rm -rf examples/resume.out
 	rm -rf examples/resume_korean.out
+
 	rm -rf examples/resume/education.log	
+
 	rm -rf examples/resume/experience_korean.log
+
 	rm -rf examples/resume/project.log	
+
 	rm -rf examples/resume.synctex.gz
 	rm -rf examples/resume_korean.synctex.gz
+	rm -rf examples/merge_resume.synctex.gz
 fi
 
 unset process_TeXShop_CNT
